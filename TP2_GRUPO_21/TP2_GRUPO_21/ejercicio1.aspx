443ace1084be70dc4a1a87dbe199dfa087ff9f2f
Andy47UTN <andres.olguin@alumnos.frgp.utn.edu.ar>
2025-04-12 16:00:35 -0300

    Agregar proyecto TPN2

---
+++ TP2_GRUPO_21/TP2_GRUPO_21/ejercicio1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ejercicio1.aspx.cs" Inherits="TP2_GRUPO_21.ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p>
+            <asp:Button ID="btnGenerarTabla" runat="server" OnClick="btnGenerarTabla_Click" Text="Generar Tabla" />
+        </p>
+        <asp:Literal ID="litTabla" runat="server"></asp:Literal>
+    </form>
+</body>
+</html>
---
+++ TP2_GRUPO_21/TP2_GRUPO_21/ejercicio1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP2_GRUPO_21
+{
+    public partial class ejercicio1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerarTabla_Click(object sender, EventArgs e)
+        {
+            
+            string tabla = "<table border='1'>"; 
+
+            tabla += "<tr><th>Producto</th><th>Cantidad</th></tr>";
+
+            for (int i = 1; i <= 3; i++)
+            {
+                tabla += "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>";
+            }
+
+            tabla += "</table>"; 
+
+            litTabla.Text = tabla; 
+        }
+    }
+}
